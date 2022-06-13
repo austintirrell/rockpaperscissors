c1da3cc68e9387b4fa0c500c870397120cb12ea6
Austin Tirrell <austinjtirrell@gmail.com>
2022-06-12 22:11:30 -0500

    some additions

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-let rps = () => Math.floor(Math.random() * 3)
+const playerDisplay = document.getElementById('playerDisplay')
+const computerDisplay = document.getElementById('computerDisplay')
+
+//returns random int between 1 and 3
+let random = () => {
+  return Math.floor(Math.random() * 3 + 1)
+}
